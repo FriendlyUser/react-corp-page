52063bb120d2d7a71da19d731388968646e230f9
dli <davidli012345@gmail.com>
2020-02-16 12:47:58 -0800

    adding buttons to main page

--- components/LandingImage.tsx
+++ components/LandingImage.tsx
@@ -3,12 +3,48 @@ import React, {useContext} from 'react';
 import {Context} from './ThemeProvider'
 import {dark} from '../theme/dark'
 import {light} from '../theme/light'
+import { DefaultButton, PrimaryButton, Stack, IStackTokens, IIconProps } from 'office-ui-fabric-react';
+export interface IButtonExampleProps {
+  // These are set based on the toggles shown above the examples (not needed in real code)
+  disabled?: boolean;
+  checked?: boolean;
+}
+
+// Example formatting
+const stackTokens: IStackTokens = { childrenGap: 40 };
+
+export const ButtonDefaultExample: React.FunctionComponent<IButtonExampleProps> = props => {
+  const { disabled, checked } = props
+  const gitGraph: IIconProps = { iconName: 'GitGraph' }
+  const mailAlert: IIconProps = { iconName: 'MailAlert'}
+  return (
+    <Stack horizontal tokens={stackTokens}>
+      <DefaultButton
+        iconProps={gitGraph}
+        text="Github"
+        href="https://github.com"
+        allowDisabledFocus
+        disabled={disabled}
+        checked={checked} />
+      <DefaultButton
+        text="Contact"
+        iconProps={mailAlert}
+        href="mailto:davidli012345@gmail.com"
+        allowDisabledFocus
+        disabled={disabled}
+        checked={checked}
+      />
+    </Stack>
+  )
+}
+
 export const LandingAnimatedText: React.FunctionComponent = (props: any) => {
     return (
       <>
         <h3 style={{ fontSize: '3rem', color: props.mainColor }}>
           Corporate Landing Page
         </h3>
+        <ButtonDefaultExample />
       </>
     )
   }
@@ -41,6 +77,10 @@ export const LandingHeroImageBG: React.FunctionComponent = () => {
           </h3>
           <h5 style={{ fontSize: '1.5rem', color: titleColor, textAlign: 'center', marginTop: 0, marginBottom: 0 }}>
             I do not have a company, but if I did I would use Corporate Landing Page.
+            <br />
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '15px' }}>
+              <ButtonDefaultExample />
+            </div>
           </h5>
         </div>
       </div>
