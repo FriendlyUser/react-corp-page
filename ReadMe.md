a8f4bc0f4ca1a09457de2ff63f75aec4e00d263d
dli <davidli012345@gmail.com>
2020-02-16 16:58:38 -0800

    updating front-end-theme with cards

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,7 @@
 ### Corporate Landing Page in React
 
 
+The is mainly a learning project for me, learning how to add key animations and use css in js via react.
 Learned don't use git lfs if you are serving files from a static site generator, only binaries, not required to run repo.
 
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Layout from '../components/Layout'
-import {LandingHeroImageBG} from '../components/LandingImage'
-
+import { LandingHeroImageBG } from '../components/LandingImage'
+import { DocumentCardBasicExample } from '../components/DocumentCardBasicExample'
+import { Context } from '../components/ThemeProvider'
+import { dark } from "../theme/dark"
 const Index: React.FC = (props: any) => {
-
+  const [state, dispatch] = useContext(Context);
+  const {theme=dark} = state
   return (
     <Layout title='Corporate Landing Page'>
       <LandingHeroImageBG {...props} />
+      <DocumentCardBasicExample theme={theme}/>
       <h3>Content Goes here</h3>
     </Layout>
   )
